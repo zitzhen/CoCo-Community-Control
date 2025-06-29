f1ca3fd21e190c56029d28ec189644b141cd9f80
oliver <xiaozhen49@outlook.com>
2025-06-30 01:46:59 +0800

    新增检查URL参数的函数 新增错误处理函数以显示错误提示

--- user/js/fetch.js
+++ user/js/fetch.js
@@ -1,5 +1,11 @@
 const avatar_img = document.getElementById("avatar_img");
 const user_name = document.getElementById("user_name");
+const error_prompt_small_windows = document.getElementById("error_prompt_small_windows");
+const error_windows = document.getElementById("error_windows");
+const Something_went_wrong_text = document.getElementById("Something_went_wrong_text");
+const error_prompt_text_one = document.getElementById("error_prompt_text_one");
+const avatar = document.getElementById("avatar");
+const Loading = document.getElementById("Loading");
 
 function get_username(usernameParam = 'username', url = window.location.href) {
   const urlObj = new URL(url);
@@ -15,4 +21,22 @@ function get_username(usernameParam = 'username', url = window.location.href) {
   return searchParams.get(usernameParam) || null;
 }
 
+function new_error(error1 = "未知错误",error2 = "未知错误"){
+    error_prompt_small_windows.style.display = '';
+    error_windows.style.display = '';
+    avatar.style.display = 'none';
+    Loading.style.display = 'none';
+    Something_went_wrong_text.innerHTML = error1;
+    error_prompt_text_one.innerHTML = error2;
+}
 
+
+function main(){
+    const username = get_username();
+    if (!username){
+        new_error("未检测到参数","请检查路径或URL中的参数")
+    }
+}
+
+
+main()
