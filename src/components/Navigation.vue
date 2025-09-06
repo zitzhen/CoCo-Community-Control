c1e23dec5f194c455cb32fc3773f0a310ff8d6ee
oliver <oliverxiaozhen@outlook.com>
2025-09-06 14:45:48 +0800

    还原标题栏

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+        <header>
+        <div class="container">
+            <h1>ZIT-CoCo-Community CoCo编辑器的小圳社区 自定义控件下载中心</h1>
+            <p>本服务由小圳社区提供</p>
+            <p>ZIT-CoCo-Community</p>
+        </div>
+    </header>
+</template>
+
+<style>
+
+header {
+    background-color: #3498db;
+    color: white;
+    padding: 1.5rem 0;
+    text-align: center;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+.container {
+    max-width: 1200px;
+    margin: 2rem auto;
+    padding: 0 1rem;
+}
+</style>
