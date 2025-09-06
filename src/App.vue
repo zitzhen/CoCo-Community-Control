5fc67a82b187c30854e845df29743d92385cfdd7
oliver <oliverxiaozhen@outlook.com>
2025-09-06 19:06:09 +0800

    还原底部

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navigation from './components/Navigation.vue'
 import Search from './components/Search.vue'
+import Bottom from './components/Bottom.vue'
 </script>
 
 <template>
   <main>
      <Navigation />
     <Search />
+    <Bottom />
   </main>
 </template>
