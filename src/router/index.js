56f4126417ed830df8c67e6ab613eba4e3ad0ec9
oliver <oliverxiaozhen@outlook.com>
2025-09-11 22:19:57 +0800

    移动文件为标准路径并修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import UserAgreement from '../views/UserAgreement.vue'
-import PrivacyPolicy from '../views/PrivacyPolicy.vue'
+import Home from '../views/index.vue'
+import UserAgreement from '../views/agreement/useragreement/index.vue'
+import PrivacyPolicy from '../views/agreement/privacypolicy/index.vue'
 
 const routes = [
   {
