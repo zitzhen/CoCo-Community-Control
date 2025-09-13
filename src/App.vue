a3520312ddcf28d7db116b2f07ef17c06235e3a4
oliver <oliverxiaozhen@outlook.com>
2025-09-14 01:22:18 +0800

    将App.vue的部分代码转移到其他位置

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-@import '../src/assets/style/home/style.css';
-@import '../src/assets/style/home/Custom_button.css';
-@import '../src/assets/style/home/Loading.css';
-@import '../src/assets/style/control/error.css';
-@import '../src/assets/style/home/pay_button.css';
-</style>
