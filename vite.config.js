998fdc60152dddb31abf43e934e5b67f22ddfe63
oliver <oliverxiaozhen@outlook.com>
2025-09-24 11:11:58 +0800

    实现预渲染 * 构建出错

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 import { resolve } from 'path'
+import VitePrerender from 'vite-plugin-prerender'
+import routes from './src/router/index.js' // 导入路由配置
 
 const useHttps = process.env.NODE_ENV !== 'production'
 
+// 提取所有路由路径
+const allRoutes = routes.map(route => route.path)
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePrerender({
+      // 使用所有的路由路径来预渲染页面
+      routes: allRoutes, // 路由路径传递给插件
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
